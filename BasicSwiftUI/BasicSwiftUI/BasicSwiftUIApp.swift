05811341d8febe1432acf1fc381de2bb3d829471
yujin <ruy203@gmail.com>
2024-01-03 20:35:00 +0900

    기본 프로젝트 생성

---
+++ BasicSwiftUI/BasicSwiftUI/BasicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSwiftUIApp.swift
+//  BasicSwiftUI
+//
+//  Created by 홍유진 on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
