b2e182b0040152b6c0c1f428c9d3c7f09fc581f9
yujin <ruy203@gmail.com>
2024-01-04 15:13:14 +0900

    리스트 모델링

--- BasicSwiftUI/BasicSwiftUI/ListView.swift
+++ BasicSwiftUI/BasicSwiftUI/ListView.swift
@@ -7,26 +7,35 @@
 
 import SwiftUI
 
+struct Fruit: Hashable {
+    let name: String
+    let matchFruitName: String
+    let price: Int
+}
+
 struct ListView: View {
     
-    private var fruits = ["apple", "banana", "cherry", "Durian", "Elder berry"]
+    var favoritFruits = [
+        Fruit(name: "Apple", matchFruitName: "Banana", price: 1000),
+        Fruit(name: "Banana", matchFruitName: "Banana", price: 3000),
+        Fruit(name: "Cherry", matchFruitName: "Durian", price: 4000),
+        Fruit(name: "Durian", matchFruitName: "Elder berry", price: 5000),
+        Fruit(name: "Elder berry", matchFruitName: "Durian", price: 6000)
+    ]
     
-    private var price = ["1000", "3000", "4000", "2400", "6000"]
+//    private var fruits = ["apple", "banana", "cherry", "Durian", "Elder berry"]
+//    private var price = ["1000", "3000", "4000", "2400", "6000"]
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(fruits, id: \.self) { fruit in
-                    VStack {
-                        LabeledContent(fruit) {
-                            Image(systemName: "arrowshape.right.fill")
-                        }
-                        
-                        Text(price[0])
-                            .frame(maxWidth: .infinity)
-                            .multilineTextAlignment(.leading)
-                            
+                ForEach(favoritFruits, id: \.self) { fruit in
+                    VStack(alignment: .leading) {
+                        Text("\(fruit.name)")
+                        Text("\(fruit.matchFruitName)")
+                        Text("\(fruit.price)")
                     }
+                    
                     
                 }
             }
